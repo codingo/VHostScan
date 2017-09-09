72eb1ca5472f4db1cbe1602648fc44576dc36213
Michael Skelton <michaelskelton@cancerqld.org.au>
2017-09-09 22:30:17 +1000

    Removing redundant code

--- VHostScan.py
+++ VHostScan.py
@@ -9,10 +9,11 @@ from lib.core.virtual_host_scanner import *
 def print_banner():
     print("+-+-+-+-+-+-+-+-+-+  v. 0.1")
     print("|V|H|o|s|t|S|c|a|n|  Developed by @codingo_ & @__timk")
-    print("+-+-+-+-+-+-+-+-+-+  https://github.com/codingo/VHostScan")
+    print("+-+-+-+-+-+-+-+-+-+  https://github.com/codingo/VHostScan\n")
 
 
 def main():
+    print_banner()
     parser = ArgumentParser()
     parser.add_argument("-t",   dest="target_hosts", required=True, help="Set a target range of addresses to target. Ex 10.11.1.1-255" )
     parser.add_argument("-w",   dest="wordlist", required=False, help="Set the wordlist to use for generated commands. Ex /usr/share/wordlist.txt", default="./wordlists/virtual-host-scanning.txt")
@@ -23,20 +24,7 @@ def main():
     parser.add_argument('--unique-depth', dest='unique_depth', type=int, help='Show likely matches of page content that is found x times (default 1).', default=1)
     arguments = parser.parse_args()
 
-    if len(sys.argv) == 1:
-        print_banner()
-        parser.error("No arguments given.")
-        parser.print_usage
-        sys.exit()
-
-    if arguments.output_directory.endswith('/' or '\\'):
-        arguments.output_directory = arguments.output_directory[:-1]
-    if arguments.target_hosts.endswith('/' or '\\'):
-        arguments.target_hosts = arguments.target_hosts[:-1]
-
-    print_banner()
-
-    scanner = virtual_host_scanner(arguments.target_hosts, arguments.output_directory, arguments.port, arguments.unique_depth, arguments.ignore_http_codes, arguments.ignore_content_length, arguments.wordlist)
+    scanner = virtual_host_scanner(arguments.target_hosts, arguments.port, arguments.unique_depth, arguments.ignore_http_codes, arguments.ignore_content_length, arguments.wordlist)
     scanner.scan()
     print(scanner.likely_matches())
 
