f2b4550a5b66d29ae5c299a66fceb1f49257def4
Tim Kent <tim@kent.id.au>
2017-09-13 15:37:39 +1000

    Added -b and -r options to override host and port used in headers (useful when pivoting). Increased version from 0.1 to 0.2.

--- VHostScan.py
+++ VHostScan.py
@@ -7,7 +7,7 @@ from lib.core.virtual_host_scanner import *
 
 
 def print_banner():
-    print("+-+-+-+-+-+-+-+-+-+  v. 0.1")
+    print("+-+-+-+-+-+-+-+-+-+  v. 0.2")
     print("|V|H|o|s|t|S|c|a|n|  Developed by @codingo_ & @__timk")
     print("+-+-+-+-+-+-+-+-+-+  https://github.com/codingo/VHostScan\n")
 
@@ -16,8 +16,10 @@ def main():
     print_banner()
     parser = ArgumentParser()
     parser.add_argument("-t",   dest="target_hosts", required=True, help="Set a target range of addresses to target. Ex 10.11.1.1-255" )
+    parser.add_argument("-b",   dest="base_host", required=False, help="Set host to be used during substitution in wordlist (default to TARGET).", default=False)
     parser.add_argument("-w",   dest="wordlist", required=False, type=str, help="Set the wordlist to use for generated commands. Ex /usr/share/wordlist.txt", default="./wordlists/virtual-host-scanning.txt")
     parser.add_argument("-p",   dest="port", required=False, help="Set the port to use (default 80).", default=80)
+    parser.add_argument("-r",   dest="real_port", required=False, help="The real port of the webserver to use in headers when not 80 (see RFC2616 14.23), useful when pivoting through ssh/nc etc (default to PORT).", default=False)
 
     parser.add_argument('--ignore-http-codes', dest='ignore_http_codes', type=str, help='Comma separated list of http codes to ignore with virtual host scans (default 404).', default='404')
     parser.add_argument('--ignore-content-length', dest='ignore_content_length', type=int, help='Ignore content lengths of specificed amount.', default=0)
@@ -39,7 +41,7 @@ def main():
     if(arguments.ignore_content_length > 0):
         print("[>] Ignoring Content length: %s" % (arguments.ignore_content_length))
 
-    scanner = virtual_host_scanner(arguments.target_hosts, arguments.port, arguments.ssl, arguments.unique_depth, 
+    scanner = virtual_host_scanner(arguments.target_hosts, arguments.base_host, arguments.port, arguments.real_port, arguments.ssl, arguments.unique_depth, 
                                    arguments.ignore_http_codes, arguments.ignore_content_length, arguments.wordlist)
     
     scanner.scan()
