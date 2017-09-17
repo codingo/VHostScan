2582a12a3a89fb2e09909c92ed67286842101e43
Michael Skelton <michaelskelton@cancerqld.org.au>
2017-09-17 16:38:24 +1000

    Added used flags to file headers

---
+++ lib/helpers/output_helper.py
@@ -0,0 +1,38 @@
+from lib.core.discovered_host import *
+from lib.helpers.file_helper import *
+import time
+
+class output_helper(object):
+    def __init__(self, scanner):
+        self.scanner = scanner
+
+    def write_normal(self, filename):
+        
+        file = file_helper(filename)
+
+        # todo: finish check_directory (needs regex to split out filename)
+        # file.check_directory(filename)
+        file.write_file(self.generate_header() + self.output_normal_likely() + self.output_normal_detail())
+
+    def output_normal_likely(self):
+        output = "\n[+] Most likely matches with a unique count of {} or less:".format(str(self.scanner.unique_depth))
+        for p in self.scanner.likely_matches(): output += "\n\t[>] {}".format(p)
+
+        return output
+
+    def output_normal_detail(self):
+        output = "\n\n[+] Full scan results"
+
+        for p in self.scanner.hosts: 
+            output += "\n\n{} (Code: {}) hash: {}".format(str(p.hostname), str(p.response_code), str(p.hash))
+            for key in p.keys: output += "\n\t{}".format(key)
+        
+        return output
+
+    def generate_header(self):
+        output = "VHostScanner Log: {} {}\n".format(time.strftime("%d/%m/%Y"), time.strftime("%H:%M:%S"))
+        output += "\tTarget: {}\n\tBase Host: {}\n\tPort: {}".format(self.scanner.target, self.scanner.base_host, self.scanner.port)
+        output += "\n\tReal Port {}\n\tIgnore HTTP Codes: {}".format(self.scanner.real_port,self.scanner.ignore_http_codes)
+        output += "\n\tIgnore Content Length: {}\n\tWordlist: {}".format(self.scanner.ignore_content_length, self.scanner.wordlist)
+        output += "\n\tUnique Depth: {}\n\tSSL: {}\n\t".format(self.scanner.unique_depth, self.scanner.ssl)
+        return output
