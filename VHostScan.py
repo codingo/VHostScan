21f232b109df4a77b4abb04c109b0e638bdebaf1
Diogo Osório <diogo.g.osorio@gmail.com>
2017-10-08 17:28:04 +0100

    Use the new scanner_argument_parser to parse the arguments
    Instead of build the argparser.ArgumentParser directly, delegate to the
    new scanner_argument_parser class the responsibility of parsing the CLI
    arguments.

--- VHostScan.py
+++ VHostScan.py
@@ -9,6 +9,7 @@ from lib.core.virtual_host_scanner import *
 from lib.helpers.output_helper import *
 from lib.helpers.file_helper import get_combined_word_lists, load_random_user_agents
 from lib.core.__version__ import __version__
+from lib.input import cli_argument_parser
 
 
 def print_banner():
@@ -19,32 +20,12 @@ def print_banner():
 
 def main():
     print_banner()
-    parser = ArgumentParser()
-    parser.add_argument("-t",   dest="target_hosts", required=True, help="Set a target range of addresses to target. Ex 10.11.1.1-255" )
-    parser.add_argument("-w",   dest="wordlists", required=False, type=str, help="Set the wordlists to use (default ./wordlists/virtual-host-scanning.txt)", default=False)
-    parser.add_argument("-b",   dest="base_host", required=False, help="Set host to be used during substitution in wordlist (default to TARGET).", default=False)
-    parser.add_argument("-p",   dest="port", required=False, help="Set the port to use (default 80).", default=80)
-    parser.add_argument("-r",   dest="real_port", required=False, help="The real port of the webserver to use in headers when not 80 (see RFC2616 14.23), useful when pivoting through ssh/nc etc (default to PORT).", default=False)
+    
+    parser = cli_argument_parser()
+    arguments = parser.parse(sys.argv[1:])
 
-    parser.add_argument('--ignore-http-codes', dest='ignore_http_codes', type=str, help='Comma separated list of http codes to ignore with virtual host scans (default 404).', default='404')
-    parser.add_argument('--ignore-content-length', dest='ignore_content_length', type=int, help='Ignore content lengths of specificed amount (default 0).', default=0)
-    parser.add_argument('--unique-depth', dest='unique_depth', type=int, help='Show likely matches of page content that is found x times (default 1).', default=1)
-    parser.add_argument("--ssl", dest="ssl",   action="store_true", help="If set then connections will be made over HTTPS instead of HTTP (default http).", default=False)
-    parser.add_argument("--fuzzy-logic", dest="fuzzy_logic", action="store_true", help="If set then fuzzy match will be performed against unique hosts (default off).", default=False)
-    parser.add_argument("--no-lookups", dest="no_lookup", action="store_true", help="Disable reverse lookups (identifies new targets and appends to wordlist, on by default).", default=False)
-    parser.add_argument("--rate-limit", dest="rate_limit", type=int, help='Amount of time in seconds to delay between each scan (default 0).', default=0)
-    parser.add_argument('--random-agent', dest='random_agent', action='store_true', help='If set, then each scan will use random user-agent from predefined list.', default=False)
-    parser.add_argument('--user-agent', dest='user_agent', type=str, help='Specify a user-agent to use for scans')
-    parser.add_argument("--waf", dest="add_waf_bypass_headers",   action="store_true", help="If set then simple WAF bypass headers will be sent.", default=False)
-    parser.add_argument("-oN",   dest="output_normal", help="Normal output printed to a file when the -oN option is specified with a filename argument." )
-    parser.add_argument("-oJ", dest="output_json", help="JSON output printed to a file when the -oJ option is specified with a filename argument." )
-    parser.add_argument("-", dest="stdin", action="store_true", help="By passing a blank '-' you tell VHostScan to expect input from stdin (pipe).", default=False)
-
-    arguments = parser.parse_args()    
     wordlist = []
-
     word_list_types = []
-
     default_wordlist = "./wordlists/virtual-host-scanning.txt" if not arguments.stdin else None
 
     if arguments.stdin:
