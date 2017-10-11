809179c97cc28930fbcf9a7a950bc95cf17cf734
Michael <886344+codingo@users.noreply.github.com>
2017-10-11 14:21:46 +1000

    PEP8 refactoring

--- setup.py
+++ setup.py
@@ -1,21 +1,23 @@
 from setuptools import find_packages, setup
 
+
 def dependencies(file):
     with open(file) as f:
         return f.read().splitlines()
 
 with open("README.md") as f:
     setup(
-            name = "VHostScan",
-            license = "GPLv3",
-            description = "A virtual host scanner that performs reverse lookups, can be used with pivot tools, detect catch-all scenarios, aliases and dynamic default pages.",
-            long_description = f.read(),
-            author = "codingo",
-            author_email = "codingo@protonmail.com",
-            url = "http://github.com/codingo/VHostScan",
-            packages = find_packages(exclude=('tests')),
-            scripts = ['VHostScan.py'],
-            install_requires=dependencies('requirements.txt'),
-            tests_require=dependencies('test-requirements.txt'),
-            include_package_data=True
-        )
+        name="VHostScan",
+        license="GPLv3",
+        description="A virtual host scanner that performs reverse lookups, "
+                    "can be used with pivot tools, detect catch-all"
+                    "scenarios, aliases and dynamic default pages.",
+        long_description=f.read(),
+        author="codingo",
+        author_email="codingo@protonmail.com",
+        url="http://github.com/codingo/VHostScan",
+        packages=find_packages(exclude=('tests')),
+        scripts=['VHostScan.py'],
+        install_requires=dependencies('requirements.txt'),
+        tests_require=dependencies('test-requirements.txt'),
+        include_package_data=True)
