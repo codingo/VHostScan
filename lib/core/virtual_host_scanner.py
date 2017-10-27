36ddf58d803ac706426a0f0224c4b5820423449c
Tim Kent <tim@kent.id.au>
2017-10-27 16:40:10 +1000

    PEP-8 fixes

--- lib/core/virtual_host_scanner.py
+++ lib/core/virtual_host_scanner.py
@@ -14,28 +14,29 @@ DEFAULT_USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) '\
                      'AppleWebKit/537.36 (KHTML, like Gecko) '\
                      'Chrome/61.0.3163.100 Safari/537.36'
 
-import requests
-
 try:
     assert requests.__version__ != "2.18.0"
     import requests.packages.urllib3.util.ssl_ as ssl_
     import requests.packages.urllib3.connection as connection
-except (ImportError,AssertionError,AttributeError):
+except (ImportError, AssertionError, AttributeError):
     import urllib3.util.ssl_ as ssl_
     import urllib3.connection as connection
 
-#print("Using requests " + requests.__version__)
+# print("Using requests " + requests.__version__)
 
 _target_host = None
+
+
 def _ssl_wrap_socket(sock, keyfile=None, certfile=None, cert_reqs=None,
                      ca_certs=None, server_hostname=None,
                      ssl_version=None, ciphers=None, ssl_context=None,
                      ca_cert_dir=None):
     return ssl_.ssl_wrap_socket(sock, keyfile=keyfile, certfile=certfile,
-                      cert_reqs=cert_reqs, ca_certs=ca_certs,
-                      server_hostname=_target_host, ssl_version=ssl_version,
-                      ciphers=ciphers, ssl_context=ssl_context,
-                      ca_cert_dir=ca_cert_dir)
+                                cert_reqs=cert_reqs, ca_certs=ca_certs,
+                                server_hostname=_target_host,
+                                ssl_version=ssl_version, ciphers=ciphers,
+                                ssl_context=ssl_context,
+                                ca_cert_dir=ca_cert_dir)
 
 connection.ssl_wrap_socket = _ssl_wrap_socket
 
