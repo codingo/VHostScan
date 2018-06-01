83457fb2f261d5c32dad252dacd5b9166872601f
linted <linted90@hotmail.com>
2018-06-01 00:05:36 -0400

    removed the unitest dependency

--- tests/helpers/test_wordlist_helper.py
+++ tests/helpers/test_wordlist_helper.py
@@ -1,10 +1,7 @@
-import unittest
 import pytest
-from unittest.mock import patch
 
 from VHostScan.lib.helpers.wordlist_helper import WordList
 from VHostScan.lib.helpers.wordlist_helper import DEFAULT_WORDLIST_FILE
-
 
 @pytest.fixture(scope='class')
 def user_wordlist(request, tmpdir_factory):
@@ -15,97 +12,104 @@ def user_wordlist(request, tmpdir_factory):
     request.cls.user_wordlist_file = str(user_wordlist_file)
     request.cls.user_wordlist = user_wordlist
 
+@pytest.fixture()
+def wordlist():
+    wordlist = WordList()
+    return wordlist
+
+@pytest.fixture(scope="module")
+def default_wordlist():
+    wordlist = []
+    with open(DEFAULT_WORDLIST_FILE, 'r') as word_file:
+        wordlist = list(word_file.read().splitlines())
+    return wordlist
 
 @pytest.mark.usefixtures('user_wordlist')
-class TestWordList(unittest.TestCase):
+class TestWordList(object):
 
-    def setUp(self):
-        self.wordlist = WordList()
-        with open(DEFAULT_WORDLIST_FILE, 'r') as word_file:
-            self.default_wordlist = list(word_file.read().splitlines())
-
-    def test_get_wordlist_from_stdin(self):
+    def test_get_wordlist_from_stdin(self, monkeypatch, wordlist):
+        print(dir(TestWordList))
         stdin_wordlist = ['keyword1', 'keyword1']
         expected_wordlist = []
         expected_wordlist.extend(stdin_wordlist)
-        with patch('VHostScan.lib.helpers.wordlist_helper.WordList.get_stdin_wordlist', return_value=stdin_wordlist):
-            wordlist, wordlist_types = self.wordlist.get_wordlist()
-            self.assertEqual(wordlist, expected_wordlist)
+        monkeypatch.setattr(WordList, 'get_stdin_wordlist', (lambda x: stdin_wordlist))
+        wordlist_result, wordlist_types = wordlist.get_wordlist()
+        assert wordlist_result == expected_wordlist
 
-    def test_get_wordlist_from_stdin_and_wordlist(self):
+    def test_get_wordlist_from_stdin_and_wordlist(self, monkeypatch, wordlist):
         stdin_wordlist = ['keyword1', 'keyword1']
         expected_wordlist = []
         expected_wordlist.extend(stdin_wordlist)
         expected_wordlist.extend(self.user_wordlist)
-        with patch('VHostScan.lib.helpers.wordlist_helper.WordList.get_stdin_wordlist', return_value=stdin_wordlist):
-            wordlist, wordlist_types = self.wordlist.get_wordlist(self.user_wordlist_file)
-            self.assertEqual(wordlist, expected_wordlist)
+        monkeypatch.setattr(WordList, 'get_stdin_wordlist', (lambda x: stdin_wordlist))
+        wordlist_result, wordlist_types = wordlist.get_wordlist(self.user_wordlist_file)
+        assert wordlist_result == expected_wordlist
 
-    def test_using_default_wordlist(self):
+    def test_using_default_wordlist(self, monkeypatch, wordlist, default_wordlist):
         stdin_wordlist = []
-        with patch('VHostScan.lib.helpers.wordlist_helper.WordList.get_stdin_wordlist', return_value=stdin_wordlist):
-            wordlist, wordlist_types = self.wordlist.get_wordlist()
-            self.assertEqual(wordlist, self.default_wordlist)
+        monkeypatch.setattr(WordList, 'get_stdin_wordlist', (lambda x: stdin_wordlist))
+        wordlist_result, wordlist_types = wordlist.get_wordlist()
+        assert wordlist_result == default_wordlist
 
-    def test_ip_using_prefix(self):
+    def test_ip_using_prefix(self, monkeypatch, wordlist):
         stdin_wordlist = ['127.0.0.1']
         prefix = 'dev-'
-        with patch('VHostScan.lib.helpers.wordlist_helper.WordList.get_stdin_wordlist', return_value=stdin_wordlist):
-            wordlist, wordlist_types = self.wordlist.get_wordlist(None, prefix)
-            self.assertEqual(wordlist, stdin_wordlist)
+        monkeypatch.setattr(WordList, 'get_stdin_wordlist', (lambda x: stdin_wordlist))
+        wordlist_result, wordlist_types = wordlist.get_wordlist(None, prefix)
+        assert wordlist_result == stdin_wordlist
 
-    def test_ip_using_suffix(self):
+    def test_ip_using_suffix(self, monkeypatch, wordlist):
         stdin_wordlist = ['127.0.0.1']
         suffix = 'test'
-        with patch('VHostScan.lib.helpers.wordlist_helper.WordList.get_stdin_wordlist', return_value=stdin_wordlist):
-            wordlist, wordlist_types = self.wordlist.get_wordlist(None,None,suffix)
-            self.assertEqual(wordlist,stdin_wordlist)
+        monkeypatch.setattr(WordList, 'get_stdin_wordlist', (lambda x: stdin_wordlist))
+        wordlist_result, wordlist_types = wordlist.get_wordlist(None,None,suffix)
+        assert wordlist_result == stdin_wordlist
 
-    def test_ipv6_using_prefix(self):
+    def test_ipv6_using_prefix(self, monkeypatch, wordlist):
         stdin_wordlist = ['::1']
         prefix = 'dev-'
-        with patch('VHostScan.lib.helpers.wordlist_helper.WordList.get_stdin_wordlist', return_value=stdin_wordlist):
-            wordlist, wordlist_types = self.wordlist.get_wordlist(None, prefix)
-            self.assertEqual(wordlist, stdin_wordlist)
+        monkeypatch.setattr(WordList, 'get_stdin_wordlist', (lambda x: stdin_wordlist))
+        wordlist_result, wordlist_types = wordlist.get_wordlist(None, prefix)
+        assert wordlist_result == stdin_wordlist
 
-    def test_ipv6_using_suffix(self):
+    def test_ipv6_using_suffix(self, monkeypatch, wordlist):
         stdin_wordlist = ['::1']
         suffix = 'test'
-        with patch('VHostScan.lib.helpers.wordlist_helper.WordList.get_stdin_wordlist', return_value=stdin_wordlist):
-            wordlist, wordlist_types = self.wordlist.get_wordlist(None,None,suffix)
-            self.assertEqual(wordlist,stdin_wordlist)
+        monkeypatch.setattr(WordList, 'get_stdin_wordlist', (lambda x: stdin_wordlist))
+        wordlist_result, wordlist_types = wordlist.get_wordlist(None,None,suffix)
+        assert wordlist_result == stdin_wordlist
 
-    def test_word_with_prefix(self):
+    def test_word_with_prefix(self, monkeypatch, wordlist):
         stdin_wordlist = ['www','www2','www3']
         expected_wordlist = stdin_wordlist + ['dev-www','dev-www2','dev-www3']
         prefix = 'dev-'
-        with patch('VHostScan.lib.helpers.wordlist_helper.WordList.get_stdin_wordlist', return_value=stdin_wordlist):
-            wordlist, wordlist_types =  self.wordlist.get_wordlist(None,prefix)
-            self.assertEqual(wordlist,expected_wordlist)
+        monkeypatch.setattr(WordList, 'get_stdin_wordlist', (lambda x: stdin_wordlist))
+        wordlist_result, wordlist_types =  wordlist.get_wordlist(None,prefix)
+        assert wordlist_result == expected_wordlist
 
-    def test_words_with_suffix(self):
+    def test_words_with_suffix(self, monkeypatch, wordlist):
         stdin_wordlist = ['www','www2','www3']
         expected_wordlist = stdin_wordlist + ['wwwtest','www2test','www3test']
         suffix = 'test'
-        with patch('VHostScan.lib.helpers.wordlist_helper.WordList.get_stdin_wordlist', return_value=stdin_wordlist):
-            wordlist, wordlist_types = self.wordlist.get_wordlist(None,None,suffix)
-            self.assertEqual(wordlist, expected_wordlist)
+        monkeypatch.setattr(WordList, 'get_stdin_wordlist', (lambda x: stdin_wordlist))
+        wordlist_result, wordlist_types = wordlist.get_wordlist(None,None,suffix)
+        assert wordlist_result == expected_wordlist
 
-    def test_words_with_host_and_prefix(self):
+    def test_words_with_host_and_prefix(self, monkeypatch, wordlist):
         stdin_wordlist = ['www.%s']
         expected_wordlist = stdin_wordlist + ['test-www.%s']
         prefix = 'test-'
-        with patch('VHostScan.lib.helpers.wordlist_helper.WordList.get_stdin_wordlist', return_value=stdin_wordlist):
-            wordlist, wordlist_types = self.wordlist.get_wordlist(None, prefix)
-            self.assertEqual(wordlist, expected_wordlist)
+        monkeypatch.setattr(WordList, 'get_stdin_wordlist', (lambda x: stdin_wordlist))
+        wordlist_result, wordlist_types = wordlist.get_wordlist(None, prefix)
+        assert wordlist_result == expected_wordlist
 
-    def test_words_with_host_and_suffix(self):
+    def test_words_with_host_and_suffix(self, monkeypatch, wordlist):
         stdin_wordlist = ['www.%s']
         expected_wordlist = stdin_wordlist + ['wwwtest.%s']
         suffix = 'test'
-        with patch('VHostScan.lib.helpers.wordlist_helper.WordList.get_stdin_wordlist', return_value=stdin_wordlist):
-            wordlist, wordlist_types = self.wordlist.get_wordlist(None,None,suffix)
-            self.assertEqual(wordlist, expected_wordlist)
+        monkeypatch.setattr(WordList, 'get_stdin_wordlist', (lambda x: stdin_wordlist))
+        wordlist_result, wordlist_types = wordlist.get_wordlist(None,None,suffix)
+        assert wordlist_result == expected_wordlist
 
 
 
