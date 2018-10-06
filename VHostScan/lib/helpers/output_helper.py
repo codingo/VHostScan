b86c31761d076e1b69586c5f13dbd1e147ff7dc3
linted <linted@users.noreply.github.com>
2018-10-05 22:07:27 -0400

    printing the hostname instead of the hash for easier to read output

--- VHostScan/lib/helpers/output_helper.py
+++ VHostScan/lib/helpers/output_helper.py
@@ -1,10 +1,13 @@
+import itertools
+import json
+import time
+
+import numpy as np
+
+from fuzzywuzzy import fuzz
+
 from ..core.discovered_host import *
 from .file_helper import *
-import time
-from fuzzywuzzy import fuzz
-import itertools
-import numpy as np
-import json
 
 
 class output_helper(object):
@@ -91,13 +94,13 @@ class output_helper(object):
         request_hashes = {}
 
         for host in self.scanner.hosts:
-            request_hashes[host.hash] = host.content
+            request_hashes[host.hash] = (host.hostname, host.content)
 
         for a, b in itertools.combinations(request_hashes.keys(), 2):
             output += "\n\t[>] {} is {}% similar to {}".format(
-                a,
-                fuzz.ratio(request_hashes[a], request_hashes[b]),
-                b
+                request_hashes[a][0],
+                fuzz.ratio(request_hashes[a][1], request_hashes[b][1]),
+                request_hashes[b][1]
             )
 
         return output
